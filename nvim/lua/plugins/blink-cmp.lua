97837b95cb268d9cd2ae3d078e87dc664c8b25ec
Will Floyd <floyd.will@gmail.com>
2025-07-20 11:02:27 -0700

    chore(nvim): update cmp menu

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -34,7 +34,19 @@ return {
     },
 
     appearance = { nerd_font_variant = 'mono' },
-    completion = { documentation = { auto_show = false } },
+    completion = {
+      documentation = {
+        auto_show = false
+      },
+      menu = {
+        draw = {
+          columns = {
+            { "label", "label_description", gap = 1 },
+            { "kind_icon", "kind", gap = 1 }
+          },
+        }
+      },
+    },
 
     sources = {
       default = { 'lsp', 'path', 'copilot' },
@@ -62,7 +74,6 @@ return {
         },
       },
     },
-
     fuzzy = { implementation = "prefer_rust_with_warning" }
   },
   opts_extend = { "sources.default" }
