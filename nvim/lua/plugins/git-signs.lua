131dcf9e3bc41a1cee0e079b36d16474292c752c
Will Floyd <will.floyd@hustle.com>
2025-07-21 23:31:08 +0000

    chore(nvim): add inline blame separator

--- nvim/lua/plugins/git-signs.lua
+++ nvim/lua/plugins/git-signs.lua
@@ -54,7 +54,7 @@ return {
           virt_text_priority = 100,
           use_focus = true,
         },
-        current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+        current_line_blame_formatter = ' <author>, <author_time:%R> - <summary>',
         sign_priority = 6,
         update_debounce = 100,
         status_formatter = nil, -- Use default
