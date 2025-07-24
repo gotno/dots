18c057ee5d9ee09487cdd792035fbf49c5749340
Will Floyd <will.floyd@hustle.com>
2025-07-24 20:26:47 +0000

    chore(nvim): add treesitter-context

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,53 +1,58 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- <- use for opts
-  opts = {
-    ensure_installed = {
-      'diff',
-      'bash',
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    main = 'nvim-treesitter.configs', -- <- use for opts
+    opts = {
+      ensure_installed = {
+        'diff',
+        'bash',
 
-      'c',
-      'cpp',
+        'c',
+        'cpp',
 
-      'html',
-      'css',
+        'html',
+        'css',
 
-      'jsdoc',
-      'javascript',
-      'typescript',
-      'json',
+        'jsdoc',
+        'javascript',
+        'typescript',
+        'json',
 
-      'lua',
-      'luadoc',
+        'lua',
+        'luadoc',
 
-      'markdown',
-      'markdown_inline',
+        'markdown',
+        'markdown_inline',
 
-      'query',
+        'query',
 
-      'vim',
-      'vimdoc',
+        'vim',
+        'vimdoc',
+      },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        -- use regex syntax and treesitter together
+        -- ruby needs it for indenting
+        additional_vim_regex_highlighting = { 'ruby' },
+      },
+      indent = {
+        enable = true,
+        disable = { 'ruby' }
+      },
+      -- incremental_selection = {
+      --   enable = true,
+      --   keymaps = {
+      --     init_selection = "gnn",
+      --     node_incremental = "grn",
+      --     scope_incremental = "grc",
+      --     node_decremental = "grm",
+      --   },
+      -- },
     },
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- use regex syntax and treesitter together
-      -- ruby needs it for indenting
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = {
-      enable = true,
-      disable = { 'ruby' }
-    },
-    -- incremental_selection = {
-    --   enable = true,
-    --   keymaps = {
-    --     init_selection = "gnn",
-    --     node_incremental = "grn",
-    --     scope_incremental = "grc",
-    --     node_decremental = "grm",
-    --   },
-    -- },
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-context', opts = {}
   },
 }
