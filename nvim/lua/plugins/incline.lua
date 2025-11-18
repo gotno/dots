f2a239c81e7d7c27ae99c943ff973810e277604b
Will Floyd <will.floyd@hustle.com>
2025-11-18 18:40:39 +0000

    chore(nvim): incline updates

--- nvim/lua/plugins/incline.lua
+++ nvim/lua/plugins/incline.lua
@@ -3,10 +3,10 @@ return {
   config = function()
     -- path builder stolen entirely from slimline
     ---@param path string
-    ---@param chars integer
-    ---@param full_dirs integer
+    ---@param num_chars_in_truncation integer
+    ---@param num_full_dirs integer
     ---@return string
-    local function truncate(path, chars, full_dirs)
+    local function truncate(path, num_chars_in_truncation, num_full_dirs)
       local parts = {}
       for part in path:gmatch('[^/]+') do
         table.insert(parts, part)
@@ -16,10 +16,10 @@ return {
       local n_parts = #parts
 
       for i, component in ipairs(parts) do
-        if i > (n_parts - full_dirs) then
+        if i > (n_parts - num_full_dirs) then
           table.insert(truncated, component)
-        elseif #component > chars then
-          table.insert(truncated, component:sub(1, chars))
+        elseif #component > num_chars_in_truncation then
+          table.insert(truncated, component:sub(1, num_chars_in_truncation))
         else
           table.insert(truncated, component)
         end
@@ -30,21 +30,32 @@ return {
 
     require('incline').setup({
       render = function(props)
+        local result = {}
+
         local path = vim.fs.normalize(vim.fn.expand('#' .. props.buf .. ':.:h'))
         local truncated_path = truncate(path, 3, 3);
         local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
-        local modified = vim.bo[props.buf].modified
-        return {
-          ' ',
-          truncated_path .. '/' .. filename,
-          modified and { ' *', guifg = '#888888', gui = 'bold' } or '',
-          ' '
-        }
+        local is_file = filename ~= ''
+
+        local unsaved = is_file and vim.bo[props.buf].modified
+
+        if unsaved then
+          table.insert(result, '* ');
+        end
+
+        table.insert(result, {
+          is_file and truncated_path .. '/' .. filename or '[no filename]',
+          gui = unsaved and 'italic' or '',
+        });
+
+        return result;
       end,
       hide = {
         cursorline = 'focused_win', -- hide when cursor is on same line in focused buffer
       },
       window = {
+        padding = 1,
+        padding_char = " ",
         winhighlight = {
           active = { Normal = 'CurSearch' },
           inactive = { Normal = 'Search' },
