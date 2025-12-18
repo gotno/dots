1fad3b99b5163b1ea1af904bc216ee4d7f3c1484
Will Floyd <will.floyd@hustle.com>
2025-12-18 18:11:46 +0000

    chore(nvim): minor blink-cmp config change

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -16,6 +16,23 @@ return {
     -- See :h blink-cmp-config-keymap for defining your own keymap
     keymap = {
       preset = 'default',
+      -- defaults:
+      -- ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+      -- ['<C-e>'] = { 'hide', 'fallback' },
+      -- ['<C-y>'] = { 'select_and_accept', 'fallback' },
+      --
+      -- ['<Up>'] = { 'select_prev', 'fallback' },
+      -- ['<Down>'] = { 'select_next', 'fallback' },
+      -- ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+      -- ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+      --
+      -- ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+      -- ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+      --
+      -- ['<Tab>'] = { 'snippet_forward', 'fallback' },
+      -- ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+      --
+      -- ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
     },
 
     appearance = { nerd_font_variant = 'mono' },
@@ -37,6 +54,7 @@ return {
         }
       },
     },
+    { signature = { enabled = true } },
 
     sources = {
       default = { 'lsp', 'path', 'copilot' },
