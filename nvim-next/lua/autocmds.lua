2dc45ef4d1baecf1e667275db6d55a0c6e2423d5
Will Floyd <will.floyd@hustle.com>
2026-02-20 21:23:48 +0000

    chore(nvim): add autocmd to trim whitespace on save

--- nvim-next/lua/autocmds.lua
+++ nvim-next/lua/autocmds.lua
@@ -8,3 +8,16 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   group = highlight_group,
   pattern = "*",
 })
+
+-- trim trailing whitespace before writing buffer
+vim.api.nvim_create_autocmd(
+  'BufWritePre',
+  {
+    callback = function ()
+      local view = vim.fn.winsaveview()
+      vim.cmd [[keepp %s/\s\+$//e]]
+      -- vim.cmd 'update' -- save if altered
+      vim.fn.winrestview(view)
+    end
+  }
+)
