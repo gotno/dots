1061609ce54f10470a675ed3807c7b02d411e9a9
Will Floyd <will.floyd@hustle.com>
2026-02-24 17:37:40 +0000

    chore(nvim): add bento for buffer management

--- nvim/lua/nav.lua
+++ nvim/lua/nav.lua
@@ -16,3 +16,22 @@ vim.keymap.set('t', '<c-j>', '<c-\\><c-n><cmd>TmuxNavigateDown<cr>')
 vim.keymap.set('t', '<c-k>', '<c-\\><c-n><cmd>TmuxNavigateUp<cr>')
 vim.keymap.set('t', '<c-l>', '<c-\\><c-n><cmd>TmuxNavigateRight<cr>')
 vim.keymap.set('t', '<c-\\>', '<c-\\><c-n><cmd>TmuxNavigatePrevious<cr>')
+
+vim.pack.add({
+  'https://github.com/serhez/bento.nvim',
+})
+require('bento').setup({
+  ui = {
+    mode = "floating", -- "floating" | "tabline"
+    floating = {
+      -- position = "middle-right", -- See position options below
+      -- offset_x = 0, -- Horizontal offset from position
+      -- offset_y = 0, -- Vertical offset from position
+      -- dash_char = "─", -- Character for collapsed dashes
+      -- border = "none", -- "rounded" | "single" | "double" | etc. (see :h winborder)
+      -- label_padding = 1, -- Padding around labels
+      minimal_menu = 'dashed', -- nil | "dashed" | "filename" | "full"
+      -- max_rendered_buffers = nil, -- nil (no limit) or number for pagination
+    },
+  },
+})
