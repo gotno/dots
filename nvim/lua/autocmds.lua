1e69f279430c1e0b40d2543f20f43e4516c05bde
Will Floyd <will.floyd@hustle.com>
2026-05-14 17:46:23 +0000

    chore(nvim): autoreload changed files

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -26,3 +26,10 @@ vim.api.nvim_create_autocmd('BufReadPost', {
     vim.bo.buftype = 'nofile'
   end,
 })
+
+-- enable autoread and set up checking triggers
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter' }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = "*",
+})
