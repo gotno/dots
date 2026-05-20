1de6ebd53bf34c3cd2b232485076449e65c422bb
Will Floyd <will.floyd@hustle.com>
2026-05-20 19:31:37 +0000

    chore(nvim): robot makes yank-git-url keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -118,6 +118,89 @@ vim.keymap.set(
   { desc = 'yank filename' }
 )
 
+local function git_yank_url(line1, line2, use_master)
+  local abs = buf_abs()
+  if abs == '' then
+    vim.notify('no file', vim.log.levels.WARN)
+    return
+  end
+
+  local file_dir = vim.fn.fnamemodify(abs, ':p:h')
+  local function git(cmd)
+    return vim.fn.system(
+      'git -C ' .. vim.fn.shellescape(file_dir) .. ' ' .. cmd .. ' 2>/dev/null'
+    ):gsub('\n', '')
+  end
+
+  local remote = git('remote get-url origin')
+  if remote == '' then
+    vim.notify('no git remote', vim.log.levels.WARN)
+    return
+  end
+
+  local rel = git('ls-files --full-name ' .. vim.fn.shellescape(abs))
+  if rel == '' then
+    vim.notify('file not tracked by git', vim.log.levels.WARN)
+    return
+  end
+
+  local branch
+  if use_master then
+    local default =
+      git('symbolic-ref refs/remotes/origin/HEAD'):match('refs/remotes/origin/(.+)')
+    if not default then
+      vim.notify('could not determine default branch', vim.log.levels.WARN)
+      return
+    end
+    git('cat-file -e refs/remotes/origin/HEAD:' .. vim.fn.shellescape(rel))
+    if vim.v.shell_error == 0 then
+      branch = default
+    else
+      vim.notify('file not in ' .. default .. ' branch', vim.log.levels.WARN)
+      return
+    end
+  else
+    branch = git('branch --show-current')
+    if branch == '' then
+      vim.notify('could not determine branch', vim.log.levels.WARN)
+      return
+    end
+  end
+
+  local host, path = remote:match('^git@([^:]+):(.+)$')
+  local url = host and ('https://' .. host .. '/' .. path) or remote
+  url = url:gsub('%.git$', '')
+
+  local line_ref =
+    (line2 and line2 ~= line1)
+      and ('#L' .. line1 .. '-L' .. line2)
+      or ('#L' .. line1)
+  local full_url = url .. '/blob/' .. branch .. '/' .. rel .. line_ref
+  vim.fn.setreg('+', full_url)
+  vim.notify('yanked ' .. full_url)
+end
+
+vim.keymap.set(
+  'n', '<leader>yg',
+  function() git_yank_url(vim.fn.line('.')) end,
+  { desc = 'yank github url (current branch)' }
+)
+vim.keymap.set(
+  'v', '<leader>yg',
+  function() git_yank_url(vim.fn.line("'<"), vim.fn.line("'>")) end,
+  { desc = 'yank github url (current branch)' }
+)
+vim.keymap.set(
+  'n', '<leader>yG',
+  function() git_yank_url(vim.fn.line('.'), nil, true) end,
+  { desc = 'yank github url (main branch)' }
+)
+vim.keymap.set(
+  'v', '<leader>yG',
+  function() git_yank_url(vim.fn.line("'<"), vim.fn.line("'>"), true) end,
+  { desc = 'yank github url (main branch)' }
+)
+
 -- `gS` to split/join argument lists
 vim.pack.add({
   'https://github.com/nvim-mini/mini.splitjoin',
